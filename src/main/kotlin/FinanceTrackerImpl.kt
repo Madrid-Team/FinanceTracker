abbd43cfc1166755918374dcf4af959f044536ce
IslamMagd <eslam752001.magdy54@gmail.com>
2025-04-09 22:45:50 +0200

    implement add transaction function

--- src/main/kotlin/FinanceTrackerImpl.kt
+++ src/main/kotlin/FinanceTrackerImpl.kt
@@ -1,10 +1,20 @@
+import java.time.LocalDate
+
 object FinanceTrackerImpl : FinanceTracker {
 
     private val _transactions: MutableList<Transaction> = mutableListOf()
     val transactions = _transactions.toList()
 
     override fun add(transaction: Transaction): Boolean {
-        TODO("Not yet implemented")
+        if (transaction.amount <= 0) return false
+        if (transaction.category.name.isBlank()) return false
+        if (transaction.date.isAfter(LocalDate.now())) return false
+
+        val nextId = if (_transactions.isEmpty()) 1 else _transactions.maxOf { it.id } + 1
+        val newTransaction = transaction.copy(id = nextId)
+        _transactions.add(newTransaction)
+        return true
+
     }
 
     override fun viewAllTransactions(): List<Transaction> {
