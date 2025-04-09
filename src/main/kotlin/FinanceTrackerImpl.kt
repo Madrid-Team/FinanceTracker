9623ec42ad443914ab39e2f777cbf520c587ae33
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-09 03:14:16 +0200

    add an edit function implementation

--- src/main/kotlin/FinanceTrackerImpl.kt
+++ src/main/kotlin/FinanceTrackerImpl.kt
@@ -3,7 +3,7 @@ import java.util.*
 object FinanceTrackerImpl : FinanceTracker {
 
     private val _transactions: MutableList<Transaction> = mutableListOf()
-    val transactions = _transactions.toList()
+    val transactions = _transactions.toMutableList()
 
     override fun add(transaction: Transaction) {
         TODO("Not yet implemented")
@@ -20,7 +20,19 @@ object FinanceTrackerImpl : FinanceTracker {
         newCategory: Category?,
         newDate: Date?
     ): Boolean {
-        TODO("Not yet implemented")
+        val index = transactions.indexOfFirst { it.id == transactionId }
+        if (index == -1) return false
+        val currentTransaction = transactions[index]
+        if (currentTransaction.amount < 0 || currentTransaction.category.name.any { !it.isLetter() }) return false
+        val updatedTransaction = currentTransaction.copy(
+            type = newType ?: currentTransaction.type,
+            amount = newAmount ?: currentTransaction.amount,
+            category = newCategory ?: currentTransaction.category,
+            date = newDate ?: currentTransaction.date
+        )
+        if (updatedTransaction == currentTransaction) return false
+        transactions[index] = updatedTransaction
+        return true
     }
 
     override fun deleteTransaction(transactionId: Int) {
