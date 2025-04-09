7312665f45d397f7dcd8522a46cd7830e0db9787
Israa Mohamed <e.mohamed@shabek.com>
2025-04-09 07:06:57 +0200

    add test case to check editing category with valid category name

--- src/main/kotlin/edit/Test.kt
+++ src/main/kotlin/edit/Test.kt
@@ -1,43 +1,49 @@
 package edit
 
 import Category
-import FinanceTrackerImpl
 
 fun main() {
-    val tracker = FinanceTrackerImpl
+
+
     check(
         name = "Edit existing transaction should return true",
-        result = tracker.editTransaction(1, newAmount = 200.0),
+        result = FinanceTrackerImpl.editTransaction(1, newAmount = 200.0),
         correctResult = true
     )
 
     check(
         name = "Try to edit non existing transaction should return false",
-        result = tracker.editTransaction(1000, newAmount = 3000.0),
+        result = FinanceTrackerImpl.editTransaction(1000, newAmount = 3000.0),
         correctResult = false
     )
 
     check(
         name = "Add invalid new amount should return false",
-        result = tracker.editTransaction(3, newAmount = -70.0),
+        result = FinanceTrackerImpl.editTransaction(3, newAmount = -70.0),
         correctResult = false
     )
 
     check(
         name = "Edit amount to zero should return false",
-        result = tracker.editTransaction(1, newAmount = 0.0),
+        result = FinanceTrackerImpl.editTransaction(1, newAmount = 0.0),
         correctResult = false
     )
 
     check(
         name = "Edit with no changes should return false",
-        result = tracker.editTransaction(1),
+        result = FinanceTrackerImpl.editTransaction(1),
         correctResult = false
     )
 
     check(
+        name = "Edit new category with valid category name should return true",
+        result = FinanceTrackerImpl.editTransaction(2, newCategory = Category("Food")),
+        correctResult = true
+    )
+
+    check(
         name = "Edit new category with special character or numbers should return false",
-        result = tracker.editTransaction(2, newCategory = Category("Food123#")),
+        result = FinanceTrackerImpl.editTransaction(2, newCategory = Category("Food123#")),
         correctResult = false
     )
 }
