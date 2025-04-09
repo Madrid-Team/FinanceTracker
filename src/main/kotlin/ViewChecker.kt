e070d43baa9af38b04f733bfad516a44964d5887
Hager Abobakr <hagarabobakr1@gmail.com>
2025-04-09 10:20:08 +0200

    test view all transactions function with two items

--- src/main/kotlin/ViewChecker.kt
+++ src/main/kotlin/ViewChecker.kt
@@ -21,32 +21,42 @@ fun main() {
         date = Date(2025 - 1900, 3, 1)
     )
     val transactions = listOf(transaction1)
+    val listOfTwoTransactions = listOf(transaction1,transaction2)
     val expected = """
- ID: 1
+   ID: 1
  Type: EXPENSES
  Amount: 50.0
  Category: Food
- Date: Sat Apr 05 00:00:00 EET 2025
+ Date: ${transaction1.date}
+ ID: 2
+ Type: INCOME
+ Amount: 1000.0
+ Category: Salary
+ Date: ${transaction2.date}
 """.trim()
     checkViewAllTransactions(
-        name = "Test viewAllTransactions with one item",
-        result = viewAllTransactions(transactions), correctResult = expected
+        name = "Test viewAllTransactions with two items",
+        result = viewAllTransactions(listOfTwoTransactions), correctResult = expected
     )
-    val emptyTransactionsList = emptyList<Transaction>()
-    checkViewAllTransactions(
-        name = "Test viewAllTransactions with empty transactions list",
-        result = viewAllTransactions(emptyTransactionsList),
-        correctResult = "No transactions found."
-    )
+//    checkViewAllTransactions(
+//        name = "Test viewAllTransactions with one item",
+//        result = viewAllTransactions(transactions), correctResult = expected
+//    )
+//    val emptyTransactionsList = emptyList<Transaction>()
+//    checkViewAllTransactions(
+//        name = "Test viewAllTransactions with empty transactions list",
+//        result = viewAllTransactions(emptyTransactionsList),
+//        correctResult = "No transactions found."
+//    )
 }
 
 
 fun checkViewAllTransactions(name: String, result: String, correctResult: String) {
     if (result == correctResult) {
-        println("Success -> $name \nExpected:$correctResult \nGot:$result")
+        println("Success -> $name \nExpected:\n$correctResult \nGot:\n$result")
         println("_________________________________________________________________")
     } else {
-        println("Failed->$name \nExpected:$correctResult \nGot:$result")
+        println("Failed->$name \nExpected:\n$correctResult \nGot:\n$result")
         println("_________________________________________________________________")
     }
 }
