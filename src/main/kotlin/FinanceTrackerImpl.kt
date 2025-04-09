1999e04582bd27f49f1cbe0db80489db37c50867
Israa Mohamed <e.mohamed@shabek.com>
2025-04-09 08:09:23 +0200

    update edit method implementation delete public transactions list property

--- src/main/kotlin/FinanceTrackerImpl.kt
+++ src/main/kotlin/FinanceTrackerImpl.kt
@@ -1,9 +1,9 @@
+import common.isValidCategory
 import java.util.*
 
 object FinanceTrackerImpl : FinanceTracker {
 
     private val _transactions: MutableList<Transaction> = mutableListOf()
-    val transactions = _transactions.toMutableList()
 
     override fun add(transaction: Transaction) {
         TODO("Not yet implemented")
@@ -20,18 +20,25 @@ object FinanceTrackerImpl : FinanceTracker {
         newCategory: Category?,
         newDate: Date?
     ): Boolean {
-        val index = transactions.indexOfFirst { it.id == transactionId }
-        if (index == -1) return false
-        val currentTransaction = transactions[index]
-        if (currentTransaction.amount < 0 || currentTransaction.category.name.any { !it.isLetter() }) return false
-        val updatedTransaction = currentTransaction.copy(
-            type = newType ?: currentTransaction.type,
-            amount = newAmount ?: currentTransaction.amount,
-            category = newCategory ?: currentTransaction.category,
-            date = newDate ?: currentTransaction.date
+
+        if (newAmount == null && newType == null && newDate == null && newCategory == null) return false
+
+        if (newAmount != null && newAmount <= 0 ) return false
+
+        if (newCategory != null && !newCategory.name.isValidCategory()) return false
+
+        val transaction = _transactions.find { it.id == transactionId } ?: return false
+
+        val transactionIndex = _transactions.indexOf(transaction)
+
+        val updatedTransaction = transaction.copy(
+            type = newType ?: transaction.type,
+            amount = newAmount ?: transaction.amount,
+            category = newCategory ?: transaction.category,
+            date = newDate ?: transaction.date
         )
-        if (updatedTransaction == currentTransaction) return false
-        transactions[index] = updatedTransaction
+
+        _transactions[transactionIndex] = updatedTransaction
         return true
     }
 
