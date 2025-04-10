f3f61823aec43d918af8c9a85982973c10f6e3b4
kareem esam <kareem.eldeen.esam@gmail.com>
2025-04-10 22:30:59 +0200

    enhance result interface and refactor EditTestCases

--- src/main/kotlin/Result.kt
+++ src/main/kotlin/Result.kt
@@ -1,4 +1,4 @@
-sealed interface Result {
-    data class Error(val cause: String) : Result
-    data object Success : Result
+sealed interface Result<out T> {
+    data class Error(val cause: String? = null) : Result<Unit>
+    data class Success<T>(val data: T) : Result<T>
 }
--- src/main/kotlin/edit/Test.kt
+++ src/main/kotlin/edit/Test.kt
@@ -1,51 +1,53 @@
 package edit
 
-import Category
+import Result
 
 fun main() {
 
 
-    check(
-        name = "Edit existing transaction should return true",
-        result = FinanceTrackerImpl.editTransaction(1, newAmount = 200.0),
-        correctResult = true
-    )
+//    check(
+//        name = "Edit existing transaction should return true",
+//        result = FinanceTrackerImpl.editTransaction(1, newAmount = 200.0),
+//        correctResult = true
+//    )
+//
+//    check(
+//        name = "Try to edit non existing transaction should return false",
+//        result = FinanceTrackerImpl.editTransaction(1000, newAmount = 3000.0),
+//        correctResult = false
+//    )
+//
+//    check(
+//        name = "Add invalid new amount should return false",
+//        result = FinanceTrackerImpl.editTransaction(3, newAmount = -70.0),
+//        correctResult = false
+//    )
+//
+//    check(
+//        name = "Edit amount to zero should return false",
+//        result = FinanceTrackerImpl.editTransaction(1, newAmount = 0.0),
+//        correctResult = false
+//    )
+//
+//    check(
+//        name = "Edit new category with valid category name should return true",
+//        result = FinanceTrackerImpl.editTransaction(2, newCategory = Category("Food")),
+//        correctResult = true
+//    )
 
-    check(
-        name = "Try to edit non existing transaction should return false",
-        result = FinanceTrackerImpl.editTransaction(1000, newAmount = 3000.0),
-        correctResult = false
-    )
-
-    check(
-        name = "Add invalid new amount should return false",
-        result = FinanceTrackerImpl.editTransaction(3, newAmount = -70.0),
-        correctResult = false
-    )
-
-    check(
-        name = "Edit amount to zero should return false",
-        result = FinanceTrackerImpl.editTransaction(1, newAmount = 0.0),
-        correctResult = false
-    )
-
-    check(
-        name = "Edit new category with valid category name should return true",
-        result = FinanceTrackerImpl.editTransaction(2, newCategory = Category("Food")),
-        correctResult = true
-    )
-
-    check(
+    check<Unit>(
         name = "Edit new category with special character or numbers should return false",
-        result = FinanceTrackerImpl.editTransaction(2, newCategory = Category("Food123#")),
-        correctResult = false
+        result = Result.Error(),
+        correctResult = Result.Error()
     )
 }
 
-fun check(name: String, result: Boolean, correctResult: Boolean) {
-    if (result == correctResult) {
-        println("Success - $name")
+fun <T> check(name: String, result: Result<T>, correctResult: Result<Unit>) {
+    val isResultSuccessful = result == correctResult
+
+    if (isResultSuccessful) {
+        println("success : $name")
     } else {
-        println("Failed - $name")
+        println("failed : $name")
     }
 }
