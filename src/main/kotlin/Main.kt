fd9b849f57d09158fa09c1377b1c83575e893b85
Ashraf El-Fallah <elfallahashraf44@gmail.com>
2025-04-11 22:43:23 +0200

    remove unnecessary code in main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,13 @@ import java.time.LocalDate
 
 fun main() {
     val tracker = FinanceTrackerImpl
-
-    val transaction1 = listOf(
-        Transaction(1, TransactionType.INCOME, 1500.0, Category("Freelance"), LocalDate.now()),
-        Transaction(2, TransactionType.INCOME, 200.0, Category("Groceries"), LocalDate.now())
+    val transaction = Transaction(
+        7,
+        TransactionType.INCOME, 1500.0,
+        Category("Freelance"),
+        LocalDate.now()
     )
-
-    val transaction2 = Transaction(7, TransactionType.INCOME, 1500.0, Category("Freelance"), LocalDate.now())
-
-    tracker.saveTransactions(transaction2)
+    tracker.saveTransactions(transaction)
 
     val loaded = loadTransactions()
     loaded.forEach {
