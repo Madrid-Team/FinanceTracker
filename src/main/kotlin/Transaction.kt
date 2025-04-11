b99d17d0d0ecaa1de9486e8fd928df02ddcd038b
Abdelrahmanibrrahem <ai4516299@gmail.com>
2025-04-11 07:16:01 +0200

    Add @Serializable notation to these classes

--- src/main/kotlin/Transaction.kt
+++ src/main/kotlin/Transaction.kt
@@ -1,10 +1,15 @@
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import model.LocalDateSerializer
 import java.time.LocalDate
 import java.util.Date
-
+@Serializable
 data class Transaction (
     val id:Int,
     val type:TransactionType,
     val amount:Double,
     val category:Category,
-    val date: Date,
+    @Serializable(with = LocalDateSerializer::class)
+    val date: LocalDate,
 )
