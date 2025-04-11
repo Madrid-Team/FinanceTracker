5b779e5a88a84e02e0a54ceb5c0d7908323c6d8d
Israa Mohamed <e.mohamed@shabek.com>
2025-04-11 22:40:01 +0200

    add Date data type to LocalDate

--- src/main/kotlin/Transaction.kt
+++ src/main/kotlin/Transaction.kt
@@ -1,9 +1,9 @@
-import java.util.Date
+import java.time.LocalDate
 
 data class Transaction (
     val id:Int,
     val type:TransactionType,
     val amount:Double,
     val category:Category,
-    val date: Date,
+    val date: LocalDate,
 )
