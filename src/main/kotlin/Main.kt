c6a4bb96211984f2ab7ced1c7c827806126a7cb3
Mohamed Ashraf <ma1261327@gmail.com>
2025-04-12 07:42:09 +0200

    add delete function in main class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,38 @@
 fun main() {
     val tracker = FinanceTrackerImpl
 }
+
+private fun deleteTransaction() {
+    println("Please enter transaction with id: ")
+
+    lable1@ do {
+        try {
+            val idTransaction = readln().toInt()
+            val transactionDeleted = FinanceTrackerImpl.deleteTransaction(idTransaction)
+            if (transactionDeleted) {
+                println("Transaction deleted is successful")
+                break@lable1
+            } else {
+                println("Transaction not found do you want to continue?(y/n)")
+                do {
+                    try {
+                        val option = readln()
+                        if (option == "n") {
+                            break@lable1
+                        } else if (option == "y") {
+                            println("Done")
+                            break
+                        }
+                    }catch (e:Exception){
+                        println("Please enter valid option: (y/n)")
+                    }
+                }while (true)
+
+            }
+        } catch (e: Exception) {
+            println("Please enter valid transaction id: ")
+        }
+    } while (true)
+
+
+}
